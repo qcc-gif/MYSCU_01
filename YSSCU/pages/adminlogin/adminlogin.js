7c0284ae62604ee33bbb495c32d7027034b5c5aa
Cuijiaqi <55613873+Rachel-Scu@users.noreply.github.com>
2021-11-19 00:19:52 +0800

    Add files via upload

--- YSSCU/pages/adminlogin/adminlogin.js
+++ YSSCU/pages/adminlogin/adminlogin.js
@@ -6,30 +6,18 @@ import api from "./../../api/api"
 
 Page({
   data: {
-    openid: "",
-    account: "", // 学号
+    account: "", // 管理员账号
     password: "",
-    agree: false,
     url: "http://127.0.0.1:3000",
-    isFollow: false
   },
 
-  onLoad(options) {
-    // let that = this
-    // app.getopenid().then(function(res){
-    //   console.log("res",res)
-    //   that.setData({
-    //     openid: res.data.openid
-    //   })
-    //   console.log(that.data)
-    //   app.globalData.openid = res.data.openid
-    //   app.globalData.session_key = res.data.session_key
-    // if(1){
-    //   wx.redirectTo({
-    //     url: './../mine/mine',
-    //   });  
-    // }
-    // })
+  onLoad: function() {
+    if (wx.getStorageSync('account')){
+      app.globalData.account = wx.getStorageSync('account');
+      wx.reLaunch({
+        url: '/pages/adminstor/adminstor',
+      })
+    }
   },
 
   onShow: function () {
@@ -37,27 +25,34 @@ Page({
     })
   },
 
-  showDetailText: function () {
-    Dialog.alert({
-      title: '用户协议',
-      message: '一、《用户协议》条款的接受\n请您认真阅读本协议，尤其是免除或者限制本平台责任的条款及其它限制您权利的条款，一旦您注册即表示您已经阅读并且同意与本平台达成协议，完全理解并接受所有的《用户协议》条款。阅读本协议过程中，如果您不同意本协议或其中任何条款约定，您应立即停止注册程序。\n二、用户注册条件\n在校大学生且同意使用手机号和获取一些内容',
-    }).then(() => {
-      // on close
-    });
+  adminnumber: function(event){
+    this.setData({
+     account: event.detail,
+    })
+    console.log('account:', this.data.account)
+  },
+
+  pwd: function(event){
+    this.setData({
+      password: event.detail,
+     })
+     console.log('pwd:', this.data.password)
   },
 
   // 点击登录
   register: function () {
-    api.post(this.data.url + '/users/login', {
+    api.post(this.data.url + 'admin/login', {
       account: this.data.account,
       pwd: this.data.password
     }).then((res) => {
       if (res.success) {
         console.log("验证成功")
         console.log(res)
-        let path = `name=${res.adminname}&image=${res.image}`
-        wx.redirectTo({
-          url: './../adminstor/adminstor?'+path,
+        // let path = `name=${res.adminname}&image=${res.image}`
+        app.globalData.account = this.data.account
+        wx.setStorageSync('account', this.data.account);  // account的本地缓存
+        wx.reLaunch({
+          url: 'pages/adminstor/adminstor',
         });
       } else {
         console.log("验证失败")
@@ -67,10 +62,4 @@ Page({
     })
   },
 
-  /* 校验是否全由13位数字组成 */
-  /* 进行学号校验 */
-  isStudentNo: function (str) {
-    var reg = /^[0-9]{13}$/;   /*定义验证表达式*/
-    return reg.test(str);     /*进行验证*/
-  }
 })
--- YSSCU/pages/login/login.js
+++ YSSCU/pages/login/login.js
@@ -1,29 +1,164 @@
 // pages/login/login.js
 const api = require('./../../api/api')
-const app = getApp();
+const app = getApp();  // 全局变量
 
 Page({
   data: {
     studentnumber: "",
-    pwd: "",
+    ans: "",
     stuerrmsg: "",
     pwderrmsg: "",
+    problem: [['三餐在江安X园（一个字）？', '西'],
+      ['创高一学期几次（数字）？', '20'],
+      ['吴玉章学院住在几舍（数字）？', '22'],
+      ['教务处选课时会崩（是/否）？', '是']],
+      k: Math.floor(Math.random()*0 + 3)
   },
-  loginIn: async() => {
-    // console.log(this.data.studentnumber, this.data.pwd)
-    // wx.switchTab({
-    //   url: './../mine/mine',
-    // })
-    let res = await api.post('http://zhjw.scu.edu.cn/j_spring_security_check',{
-      'j_username':'201914146032',
-      'j_password':'0a06dc7fa59dc8d1038d98839019ea31',
+
+  onLoad: function() {
+      if (wx.getStorageSync('studentnumber')){  // 用户不用重复登录
+        app.globalData.studentnumber = wx.getStorageSync('studentnumber');
+        wx.reLaunch({
+          url: '/pages/mine/mine',
+        })
+      }
+  },
+
+  getUserInfo:function(){
+    console.log('getUserInfo')
+    return new Promise((resolve,reject) => {
+      wx.getUserProfile({
+        desc: '用户完善个人资料', 
+        success: (res) => {
+          console.log('sucess', res),
+          resolve(res)
+        },
+        fail:(err) => {
+          reject(err)
+        }
+      })
     })
-    console.log(res)
   },
-  onLoad: async () => {
-    // let res = await app.getopenid()
-    // console.log(res)
-    let res = await api.get('http://zhjw.scu.edu.cn/img/captcha.jpg?4')
-    console.log(res)
-  }
+
+  getLogin:function(){
+    api.post(this.data.url + '/users/login', {
+      studentnumber: this.data.studentnumber,
+    }).then((res) => {
+      if (res.success) {
+        console.log("认证成功"),
+        app.globalData.studentnumber = this.data.studentnumber
+        wx.setStorageSync('studentnumber', studentnumber)
+        wx.reLaunch({
+          url: '/pages/mine/mine',
+        });
+      } else {
+        console.log("认证失败")
+      }
+    }).catch((err) => {
+      console.log("err", err)
+    })
+  },
+
+  // 登录
+  register: function(){
+    // 校验学号和问题的答案
+    // this.checkStudentNumber()
+    // this.checkAns()
+    
+    // 用户同意后登录
+    let userRes = this.getUserInfo()
+    let loginRes = this.getLogin()
+    console.log(userRes)
+    Promise.all([userRes,loginRes]).then((res) => {
+      console.log('成功', res)
+    })
+  },
+
+  toAdmin: function(){
+    console.log('点击了管理员登录')
+    wx.reLaunch({
+      url: '/pages/adminlogin/adminlogin',
+    });
+  },
+
+  // 获取输入的学号
+  stuno: function(event){
+    this.setData({
+      studentnumber: event.detail,
+    })
+    console.log('stuno:', this.data.studentnumber)
+  },
+
+  // 获取输入的答案
+  ans: function(event){
+    this.setData({
+      ans: event.detail,
+    })
+    console.log('stuno:', this.data.ans)
+  },
+
+  // 校验学号
+  checkStudentNumber: function(){
+    if(!this.data.studentnumber){
+      wx.showModal({
+        title: '学号输入错误',
+        content: '学号不能为空！',
+        success (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    }
+
+    let reg = /^20(0|1)[0-9]{10}$/
+    if(!reg.test(this.data.studentnumber)){
+      wx.showModal({
+        title: '学号输入错误',
+        content: '请检查您的学号！',
+        success (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    }
+  },
+
+  // 校验答案
+  checkAns: function(){
+    var index = this.data.k;
+    var ans = this.data.problem[index][1];
+    if(!ans){
+      wx.showModal({
+        title: '回答错误',
+        content: '请填写问题的回答！',
+        success (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    }
+
+    if(ans!=this.data.ans){
+      wx.showModal({
+        title: '回答错误',
+        content: '请检查您的回答！',
+        success (res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          } else if (res.cancel) {
+            console.log('用户点击取消')
+          }
+        }
+      })
+    }
+  },  
 })
