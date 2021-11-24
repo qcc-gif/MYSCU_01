80593612f85f84a6ef2b689ea8445eb0718c29e3
hmt1314luck <56394496+hmt1314luck@users.noreply.github.com>
2021-11-24 22:15:48 +0800

    Add files via upload

--- YSSCU/pages/accountManage/accountManage.js
+++ YSSCU/pages/accountManage/accountManage.js
@@ -1,17 +1,42 @@
 // pages/accountManage/accountManage.js
+var app = getApp();
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        complaintList:[{id:'1',
+        userId: '1',
+        profilePhoto: 'https://img.yzcdn.cn/vant/cat.jpeg',
+        name:  '大白',
+          position:'教学楼 一教',
+          time:'2000年11月14日 14:00',
+          reason:  "言论过激，以后注意。"
+          }]
     },
+    search:function(){
+        var navigateId=1;
+        wx.navigateTo({
+        url:'/pages/searchUser/searchUser?navigateId='+navigateId
+      });
+},
+   
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function () {
+    //     let url = 'url'
+    // // 请求申诉列表
+    // api.post(url, {  
+      
+    // }).then((res) => {
+    //    //展示申诉列表
+    //   this.setData({
+    //    complaintList：res.data
+    //   })
+    // })
 
     },
 
@@ -63,4 +88,6 @@ Page({
     onShareAppMessage: function () {
 
     }
+    
+       
 })
