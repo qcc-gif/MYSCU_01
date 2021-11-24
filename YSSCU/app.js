d5de4c11531c2e9e31fa295d54290fe4c277983c
Cuijiaqi <55613873+Rachel-Scu@users.noreply.github.com>
2021-11-25 00:40:05 +0800

    Add files via upload

--- YSSCU/app.js
+++ YSSCU/app.js
@@ -2,38 +2,40 @@
 App({
   globalData: {
     userInfo: null,
-    host: "",
-    openid: "ofktd44z-FOuIvGSMrhgBwqvCNFk",
+    openid: "",
+    studentnumber: "",
     account: "",
+    avatar: "",
+    name: "",
     url: "http://10.133.133.26:3000",
   },
 
-  getopenid: async () => {
-    return new Promise(async (resolve, reject) => {
-      wx.login({
-        success: res => {
-          // 发送 res.code 到后台换取 openId, sessionKey, unionId
-          var code = res.code;
-          var appid = 'wx608c38127256116c'; // 微信小程序的appid
-          var secret = '853f8491160ddc7e41b947831d51395b'; // 小程序secret
-          wx.request({
-            url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code',
-            data: {
-              studentnumber: studentnumber,
-            },
-            header: {
-              'content-type': 'application/json'
-            },
-            success: (res) => {
-              resolve(res)  // 获取到用户的open_id
-              wx.setStorageSync('openid', res.data.openid); // 缓存到本地
-            },
-            fail: (err) => {
-              reject(err)
-            }
-          })
-        }
-      })
-    })
-  }
+  // getopenid: async () => {
+  //   return new Promise(async (resolve, reject) => {
+  //     wx.login({
+  //       success: res => {
+  //         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+  //         var code = res.code;
+  //         var appid = 'wx608c38127256116c'; // 微信小程序的appid
+  //         var secret = '853f8491160ddc7e41b947831d51395b'; // 小程序secret
+  //         wx.request({
+  //           url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code',
+  //           data: {
+  //             studentnumber: studentnumber,
+  //           },
+  //           header: {
+  //             'content-type': 'application/json'
+  //           },
+  //           success: (res) => {
+  //             resolve(res)  // 获取到用户的open_id
+  //             wx.setStorageSync('openid', res.data.openid); // 缓存到本地
+  //           },
+  //           fail: (err) => {
+  //             reject(err)
+  //           }
+  //         })
+  //       }
+  //     })
+  //   })
+  // }
 })
