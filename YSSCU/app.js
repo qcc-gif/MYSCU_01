85a88c94006a145a8e7d3bf4336a459b601b6d04
Cuijiaqi <55613873+Rachel-Scu@users.noreply.github.com>
2021-11-30 23:59:55 +0800

    Add files via upload

--- YSSCU/app.js
+++ YSSCU/app.js
@@ -1,41 +1,15 @@
 // app.js
 App({
   globalData: {
-    userInfo: null,
-    openid: "",
-    studentnumber: "",
-    account: "",
-    avatar: "",
-    name: "",
+    userInfo: null,                     // 用户信息
+    openid: "",                         // 用户的openid
+    studentnumber: "",                  // 川大学号
+    avatarUrl: "",                         // 用户头像
+    nickName: "",                       // 用户昵称
+    account: "",                        // 管理员账号
+    name: "",                           // 管理员名字
+    img: "",                            // 管理员头像
     url: "http://10.133.133.26:3000",
   },
 
-  // getopenid: async () => {
-  //   return new Promise(async (resolve, reject) => {
-  //     wx.login({
-  //       success: res => {
-  //         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-  //         var code = res.code;
-  //         var appid = 'wx608c38127256116c'; // 微信小程序的appid
-  //         var secret = '853f8491160ddc7e41b947831d51395b'; // 小程序secret
-  //         wx.request({
-  //           url: 'https://api.weixin.qq.com/sns/jscode2session?appid=' + appid + '&secret=' + secret + '&js_code=' + code + '&grant_type=authorization_code',
-  //           data: {
-  //             studentnumber: studentnumber,
-  //           },
-  //           header: {
-  //             'content-type': 'application/json'
-  //           },
-  //           success: (res) => {
-  //             resolve(res)  // 获取到用户的open_id
-  //             wx.setStorageSync('openid', res.data.openid); // 缓存到本地
-  //           },
-  //           fail: (err) => {
-  //             reject(err)
-  //           }
-  //         })
-  //       }
-  //     })
-  //   })
-  // }
 })
