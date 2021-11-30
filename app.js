de51bb2fbf1e02a03280865fd0d189c58d93e984
Cuijiaqi <55613873+Rachel-Scu@users.noreply.github.com>
2021-11-30 21:50:31 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+App({
+  globalData: {
+    userInfo: null,                     // 用户信息
+    openid: "",                         // 用户的openid
+    studentnumber: "",                  // 川大学生的学号
+    account: "",                        // 管理员账号
+    avatar: "",                         // 头像
+    name: "",                           // 管理员名字
+    url: "http://10.133.133.26:3000",
+  },
+
+})
