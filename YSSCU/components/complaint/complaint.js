f69d9f6b92bd8547ac1b965b556dcb7b0e8c2ef9
Cuijiaqi <55613873+Rachel-Scu@users.noreply.github.com>
2021-12-06 22:47:43 +0800

    almost finished

--- YSSCU/components/complaint/complaint.js
+++ YSSCU/components/complaint/complaint.js
@@ -1,84 +1,81 @@
 // components/complaint/complaint.js
+
 Component({
-    /**
-     * 组件的属性列表
-     */
-    properties: {
-      studentNumber:{
-        type: Number,
-        value: 20191414
+  properties: {
+    studentNumber:{                                 // 学号
+      type: Number,
+      value: 20191414
+    },
+    profilePhoto:{                                 // 头像
+      type:String,
+      value: 'https://img.yzcdn.cn/vant/cat.jpeg'
+    },
+    name: {                                       // 名字
+      type: String,
+      value: '大白'
+    },
+    time: {                                      // 时间
+      type: String,
+      value: '2000年11月14日 14:00'
+    },
+    atype: {                                      // 理由
+      type: String,
+      value: "言论过激，以后注意。"
+    },
+    areason: {                                   // 阐述细节
+      type: String,
+      value: "言论过激，以后注意。"
+    },
+    aphone: {                                     // 联系方式
+      type: String,
+      value: "言论过激，以后注意。"
+    }
+  },
+
+  data: {
+    isAgree: false                              // false拒绝，true同意
+  },
+
+  methods: {
+    switchChange(e) {
+        this.setData({
+         isAgree: e.detail.value
+        })
+        console.log(this.data)
       },
-        userId:{
-            type:Number,
-            value: 1
-        },
-        profilePhoto:{
-            type:String,
-            value: 'https://img.yzcdn.cn/vant/cat.jpeg'
-        },
-        name: {
-            type: String,
-            value: '大白'
-          },
-          time: {
-            type: String,
-            value: '2000年11月14日 14:00'
-          },
-          reason: {
-            type: String,
-            value: "言论过激，以后注意。"
-          }
 
-    },
+      // 点击发送
+      sendClick(){
+        var that=this
+        wx.showLoading({
+          title: '正在发送...',
+        });
+        
+        // 判断是否解冻
+        if(isAgree){
+          var url = app.globalData.url + ''//同意解冻
+        }else{
+          var url = app.globalData.url + ''//拒绝解冻
+        }
 
-    /**
-     * 组件的初始数据
-     */
-    data: {
-        isAgree:''//false拒绝，true同意
-
-    },
-
-    /**
-     * 组件的方法列表
-     */
-    methods: {
-        switchChange(e) {
-            var sw = e.detail.value
-            this.setData({
-             isAgree:sw
+        api.post(url, {
+          studentNumber:this.data.studentNumber
+        }).then((res) => {
+          if(res.data.success){
+            wx.showToast({
+              title: '发送成功！',
+              duration: 3000,
             })
-            console.log(this.data)
-          },
-          sendClick(){
-            var that=this
-              wx.showLoading({
-                title: '正在发送...',
-              });
-              if(isAgree){
-                let url = app.globalData.url + ''//同意解冻
-              }else{
-                let url = app.globalData.url + ''//拒绝解冻
-              }
-              api.post(url, {
-                openid: wx.getStorageInfoSync('openid'),
-                studentNumber:this.data.studentNumber
-              }).then((res) => {
-                if(res.data.success){
-                    wx.showToast({
-                        title: '发送成功！',
-                        
-                    })
-                    console.log(res.data);
-                }
-                else{
-                    wx.showToast({
-                        title: '发送失败！',
-                    })
-                }
-              })
-            },
-
+            console.log(res.data);
+          }else{
+            wx.showToast({
+              title: '发送失败！',
+              icon: 'none',
+              duration: 3000,
+            })
+          }
+        })
+      },
 
     }
 })
--- YSSCU/components/frozenAccount/frozenAccount.js
+++ YSSCU/components/frozenAccount/frozenAccount.js
@@ -5,29 +5,44 @@ const app = getApp();
 Component({
   properties: {
     studentNumber:{
-      type: String,
-      value: '20191414'
-    },
-    profilePhoto:{
-      type:String,
-      value: 'https://img.yzcdn.cn/vant/cat.jpeg'
-    },
-    name: {
-      type: String,
-      value: '大白'
-    },
-    isFrozen: {         // 冻结状态：false不冻结，true冻结
-      type: Boolean,
-      value: false
-    },
-    checkChange: {    // 确认管理员有没有在提交前改变用户账号状态
-      type: Boolean,
-      value: false
-    }
+    type: String,
+    value: '20191414'
+  },
+  profilePhoto:{
+    type:String,
+    value: 'https://img.yzcdn.cn/vant/cat.jpeg'
+  },
+  name: {
+    type: String,
+    value: '大白'
+  },
+  isFrozen: {                           // 冻结状态：false不冻结，true冻结
+    type: Boolean,
+    value: false
+  },
+  checkChange: {                       // 确认管理员有没有在提交前改变用户账号状态
+    type: Boolean,
+    value: false
+  }
   },
 
   data: {
     url: "",
+  },
+
+  lifetimes: {
+    attached: function () {
+      let url = app.globalData.url + '/?'
+      api.post(url, {
+        studentNumber: this.data.studentNumber,
+      }).then((res) => {
+        if(!res.data.empty){
+          this.setData({
+            isFrozen: res.data.isFrozen,
+          })
+        }
+      })
+    }
   },
 
   methods: {
@@ -46,26 +61,41 @@ Component({
       if(checkChange){  // 如果有改变账号状态
         if(this.properties.isFrozen){ // 如果需要冻结账户
           var url = app.globalData.url + ''
+          api.post(url, {
+            "studentnumber": this.properties.studentNumber,
+          }).then((res) => {
+            if(res.data.success){
+              wx.showToast({
+                title: '发送成功！',
+                duration: 3000,
+              })
+            }else{
+              wx.showToast({
+                title: '发送失败！',
+                icon: 'none',
+                duration: 3000,
+              })
+            }
+          })
         }else{  // 如果需要解冻账户
           var url = app.globalData.url + ''
+          api.post(url, {
+            "studentnumber": this.properties.studentNumber,
+          }).then((res) => {
+            if(res.data.success){
+              wx.showToast({
+                title: '发送成功！',
+                duration: 3000,
+              })
+            }else{
+              wx.showToast({
+                title: '发送失败！',
+                icon: 'none',
+                duration: 3000,
+              })
+            }
+          })
         }
-         // 发送请求
-        api.post(url, {
-          "studentnumber": this.properties.studentNumber,
-          'isFrozen': this.data.isFrozen,
-        }).then((res) => {
-          if(res.data.success){
-            wx.showToast({
-              title: '发送成功！',
-            })
-            console.log(res.data);
-          }else{
-            wx.showToast({
-              title: '发送失败！',
-              icon: 'none',
-            })
-          }
-        })
       }else{   // 没有有改变账号状态
         wx.showToast({
           title: '无状态改变！',
@@ -73,6 +103,6 @@ Component({
         })
       }
     },
-
+    
   }
 })
