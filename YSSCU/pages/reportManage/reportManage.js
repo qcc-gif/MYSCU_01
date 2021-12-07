444356f5d548ae99096cb37f5127f94c5174fcfc
hmt1314luck <56394496+hmt1314luck@users.noreply.github.com>
2021-12-07 11:55:10 +0800

    Add files via upload

--- YSSCU/pages/reportManage/reportManage.js
+++ YSSCU/pages/reportManage/reportManage.js
@@ -3,21 +3,37 @@ const api = require("../../api/api")
 const app = getApp();
 Page({
   data: {
-    reportList:[{id:'1',
-      userId: '1',
+    navTab: ["帖子", "评论"],  // 顶部导航栏的内容
+    currentNavtab: 0,         // 目前的下标（0是帖子，1是评论）
+    postList:[{postId:'1',
       name: '大',
       studentNumber:'2019',
       profilePhoto:'https://img.yzcdn.cn/vant/cat.jpeg',
       position:'教学楼 一教',
       time: '2000年11月14日 14:00',
-      detail: "这里是我发的帖子"
+      detail: "这里是我发的帖子",
+      reason:"不实信息"
+      }
+    ],
+    commentList:[{postId:'1',
+      name: '大',
+      studentNumber:'2019',
+      profilePhoto:'https://img.yzcdn.cn/vant/cat.jpeg',
+      position:'教学楼 一教',
+      time: '2000年11月14日 14:00',
+      detail: "这里是我发的帖子",
+      reason:"不实信息"
       }
     ]
 
   },
-  onLoad: function () {
-
-    },
+   // 切换顶部导航栏
+   switchTab: function(e){
+    this.setData({
+      currentNavtab: e.currentTarget.dataset.idx
+    });
+    this.onShow(currentNavtab);
+   },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
@@ -29,16 +45,32 @@ Page({
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {
-            let url = app.globalData.url+'url'
-    // 请求举报列表
-    api.post(url, {  
-      openid: wx.getStorageInfoSync('openid')
-    }).then((res) => {
-       //展示举报列表
-      this.setData({
-       reportList:res.data
-      })
-    })
+    onShow: function (currentNavtab) {
+          if(currentNavtab==0){
+          let url = app.globalData.url+'/admin/reportManage'
+        // 请求举报帖子列表
+        api.post(url, {  
+          
+        }).then((res) => {
+          //展示举报帖子列表
+          this.setData({
+          //postList:res.data.accountList,
+          commentList:null
+          })
+        })
+      }
+      else{
+        let url = app.globalData.url+'/admin/reportManage'
+        // 请求举报评论列表
+        api.post(url, {  
+          
+        }).then((res) => {
+          //展示举报评论列表
+          this.setData({
+          //commentList:res.data.accountList
+          postList:null
+          })
+        })
+      }
     },
 })
